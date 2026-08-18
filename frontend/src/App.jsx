7a48ed2526a5069f30cd0369e72668584741dfba
Vayun Hirthik <vayunhirthik2410704@ssn.edu.in>
2026-08-18 23:15:59 +0530

    start of the front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,35 @@
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+import { Button } from '@heroui/react';
+import {WallpaperProvider} from "./context/WallpaperContext";
+import {ThemeProvider} from "./context/ThemeContext";
+import { Navigate,Routes,Route } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import {useAuth} from "@clerk/react";
 
 function App() {
 
+  const {isSignedIn,isLoaded}=useAuth();
+
+  if(!isLoaded){
+    return(
+      <p>loading...</p>
+    )
+  }
+
   return (
-    <>
-    <div>
-      <h1>MY app</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton  mode="modal"/>
-          <SignUpButton  mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
     
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage/>: <Navigate to={"/auth"} replace /> }/>
+          <Route path="/auth" element={!isSignedIn ? <AuthPage/>: <Navigate to={"/chat"} replace/>}/>
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
+    
+    
+    
     
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from "react-router";
 
 import { ClerkProvider } from '@clerk/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider>
-      <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
     </ClerkProvider>
-    
   </StrictMode>,
 )
