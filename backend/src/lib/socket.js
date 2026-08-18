073430bc4296ed2b8ab075f6fc2dfd77b922cb95
Vayun Hirthik <vayunhirthik2410704@ssn.edu.in>
2026-08-18 21:12:59 +0530

    s=>S

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import {server} from "socket.io";
+import {Server} from "socket.io";
 
 const app = express();
 const server=http.createServer(app);
